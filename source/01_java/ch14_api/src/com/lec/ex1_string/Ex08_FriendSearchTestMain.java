35d420a1ae160324185ccd8000aaf5a3d6d5cecb
unknown <spacenyi@naver.com>
2022-03-28 14:26:37 +0900

    03-28

--- source/01_java/ch14_api/src/com/lec/ex1_string/Ex08_FriendSearchTestMain.java
+++ source/01_java/ch14_api/src/com/lec/ex1_string/Ex08_FriendSearchTestMain.java
@@ -1,5 +1,33 @@
 package com.lec.ex1_string;
 
-public class Ex08_FriendSearchTestMain {
+import java.util.Scanner;
 
-}
+public class Ex08_FriendSearchTestMain {
+	public static void main(String[] args) {
+		Friend[] friends = {new Friend("홍길동", "010-9999-9999", "05-22"),
+				new Friend("신길동", "010-8888-8888", "09-22"),
+				new Friend("마길동", "010-8888-9999", "08-22"),
+				new Friend("윤길동", "010-7777-7777", "04-22"),
+				new Friend("김길동", "010-6666-7777", "03-22")};
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.print("검색하고자 하는 친구의 전화번호 뒷자리는 ?");
+			String searchTel = sc.next() ; // 사용자로부터 전화번호 뒷번호 입력 받은 데이터 할당
+			if(searchTel.equals("x")) break;
+			boolean searchOk = false;
+			for(int idx=0 ; idx<friends.length ; idx++) {
+				// idx번째의 Friend객체 전화번호 뒷자리 추출
+				String tel = friends[idx].getPhone();
+				String post = tel.substring(tel.lastIndexOf('-')+1);
+				if(post.equals(searchTel)) {
+					System.out.println(friends[idx]);
+					searchOk = true;
+				}// if - 전화번호를 찾을 경우 더이상 검색하지 않고 for문 빠져나감.
+			}//for
+			if(! searchOk) {
+				System.out.println("검색하신 친구는 조회되지 않습니다.");
+			}// if
+		}//while
+		sc.close();
+	}//main
+}//class
--- source/01_java/ch14_api/src/com/lec/ex1_string/Friend.java
+++ source/01_java/ch14_api/src/com/lec/ex1_string/Friend.java
@@ -2,19 +2,19 @@ package com.lec.ex1_string;
 
 public class Friend {
 	private String name;
-	private String tel;
+	private String phone;
 	private String birthday;
 	public Friend() { }
-	public Friend(String name, String tel, String birthday) {
+	public Friend(String name, String phone, String birthday) {
 		this.name = name;
-		this.tel = tel;
+		this.phone = phone;
 		this.birthday = birthday;
 	}
 	@Override
 	public String toString() {
-		return "\n이  름 :" + name + "\n전  화 : " + tel + "\n생  일 : " + birthday;
+		return "\n이  름 :" + name + "\n전  화 : " + phone + "\n생  일 : " + birthday;
 	}
 	public String getName() { return name; }
-	public String getTel() { return tel; }
+	public String getPhone() {return phone;}
 	public String getBirthday() { return birthday; }
 }
