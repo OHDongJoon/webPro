85bdc4aa28f65b339cc479ca1a8f9a2fe83645ad
unknown <spacenyi@naver.com>
2022-03-28 17:03:06 +0900

    03-28

--- source/01_java/ch14_api/src/com/lec/ex2_date/Ex02_greagorinCalendar.java
+++ source/01_java/ch14_api/src/com/lec/ex2_date/Ex02_greagorinCalendar.java
@@ -1,8 +1,10 @@
 package com.lec.ex2_date;
 import java.util.Calendar;
-public class Ex01_calendar {
+import java.util.GregorianCalendar;
+public class Ex02_greagorinCalendar {
 	public static void main(String[] args) {
-		Calendar cal = Calendar.getInstance(); // 싱글톤 클래스
+//		Calendar cal = Calendar.getInstance(); // 싱글톤 클래스
+		GregorianCalendar cal = new GregorianCalendar();
 		System.out.println(cal); // 가독성 저하
 		int year  = cal.get(Calendar.YEAR); // 년도
 		int month = cal.get(Calendar.MONTH)+1; // 월(Calendar.MONTH) 0,1,2,3...11월
---
+++ source/01_java/ch14_api/src/com/lec/ex2_date/Ex03_gc_term.java
@@ -0,0 +1,8 @@
+package com.lec.ex2_date;
+import java.util.GregorianCalendar;
+// 두시점의 시간 계산
+public class Ex03_gc_term {
+	public static void main(String[] args) {
+		GregorianCalendar 
+	}
+}
