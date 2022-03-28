5fd9ed2f26b13a9a2a23e153b8dd7feb4d98c450
unknown <spacenyi@naver.com>
2022-03-28 12:19:58 +0900

    03-28

--- source/01_java/ch14_api/src/com/lec/ex1_string/Ex07_searchTel.java
+++ source/01_java/ch14_api/src/com/lec/ex1_string/Ex07_searchTel.java
@@ -6,18 +6,18 @@ public class Ex07_searchTel {
 		String[] tels = {"010-9999-9999", "02-8888-8888", "010-7777-8888"};
 		Scanner sc = new Scanner(System.in);
 		String searchTel ; // 사용자로부터 전화번호 뒷번호 입력 받은 데이터 할당
-		int idx = 0;
+		boolean searchOk = false;
 		System.out.print("검색하고자 하는 회원의 전화번호 뒷자리는 ?");
 		searchTel = sc.next(); // 1234
-		for(idx=0 ; idx<tels.length ; idx++) {
+		for(int idx=0 ; idx<tels.length ; idx++) {
 			// idx번째의 전화번호 뒷자리 추출
 			String post = tels[idx].substring(tels[idx].lastIndexOf('-')+1);
 			if(post.equals(searchTel)) {
 				System.out.println("검색하신 전화번호는 "+ tels[idx] +"입니다");
-				break;
+				searchOk = true;
 			}// if - 전화번호를 찾을 경우 더이상 검색하지 않고 for문 빠져나감.
 		}//for
-		if(idx == tels.length) { // saerchTel이 배열 전화에 없어서 못찾고 idx가 끝까지 간 경우
+		if(! searchOk) { // saerchTel이 배열 전화에 없어서 못찾고 idx가 끝까지 간 경우
 			System.out.println("검색하신 전화번호는 없습니다");
 		}
 	}
