309719b326639767c52cca880d7cf2bb21fe0c05
unknown <spacenyi@naver.com>
2022-03-31 10:55:11 +0900

    03-31

--- source/01_java/ch14_api/src/com/lec/ex3_math/Ex03_random.java
+++ source/01_java/ch14_api/src/com/lec/ex3_math/Ex03_random.java
@@ -1,12 +1,19 @@
 package com.lec.ex3_math;
+import java.util.Random;
+/* 0 <= Math.random()*45 < 45 
+ * 0~44까지의 정수 난수 : (int)(Math.random()*45) 
+ * 1~45까지의 정수 난수 : (int)(Math.random()*45) +1
+ * */
 public class Ex03_random {
 	public static void main(String[] args) {
 		System.out.println("0~ 45미만의 double 난수 : " + Math.random()*45);
 		System.out.println("0~ 45미만의 정수 난수 : " + (int)(Math.random()*45) );
 		System.out.println("로또 번호 뽑기 : " + ( (int)(Math.random()*45) +1 ) );
-		 /* 0 <= Math.random()*45 < 45 
-		 * 0~44까지의 정수 난수 : (int)(Math.random()*45) 
-		 * 1~45까지의 정수 난수 : (int)(Math.random()*45) +1
-		 * */
+		Random random = new Random();
+		System.out.println("int 난수 : " + random.nextInt());
+		System.out.println("double 난수 : " + random.nextDouble());  // Math.random() 유사
+		System.out.println("T/F 중 하나 난수 : " + random.nextBoolean());
+		System.out.println("0~44사이의 정수 난수 : " + random.nextInt(45)); // 0~ 45미만의 정수 난수
+		System.out.println("로또 번호 뽑기 : " + (random.nextInt(45)+1)   );
 	}
 }
