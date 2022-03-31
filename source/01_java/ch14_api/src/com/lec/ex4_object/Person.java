bd4a286dd21cdb28f223f329792642931f2badf1
unknown <spacenyi@naver.com>
2022-03-31 16:29:09 +0900

    03-31

---
+++ source/01_java/ch14_api/src/com/lec/ex4_object/Person.java
@@ -0,0 +1,11 @@
+package com.lec.ex4_object;
+public class Person {
+	private long juminNo; // 9512121025252
+	public Person(long juminNo) {
+		this.juminNo = juminNo;
+	}
+	@Override
+	public String toString() {
+		return "Person [juminNo=" + juminNo + "]";
+	}
+}
