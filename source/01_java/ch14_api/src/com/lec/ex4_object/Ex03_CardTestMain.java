3e4fea2069ebc39b31eb4b4258f1d89cd3fdf8c8
unknown <spacenyi@naver.com>
2022-03-31 17:45:33 +0900

    03-31

--- source/01_java/ch14_api/src/com/lec/ex4_object/Ex03_CardTestMain.java
+++ source/01_java/ch14_api/src/com/lec/ex4_object/Ex03_CardTestMain.java
@@ -3,8 +3,8 @@ import java.util.Random;
 // comCard는 랜덤카드 선택하여 배열방 0~끝방까지 일치하는지 불일치 하는지 구현해 봅니다.
 public class Ex03_CardTestMain {
 	public static void main(String[] args) {
-		Card[] cards = {new Card('♧', 1),  new Card('◇', 2), 
-					new Card('◇', 3), new Card('◇', 4)};
+		Card[] cards = {new Card('♣', 1),  new Card('◆', 2), 
+					new Card('◆', 3), new Card('◆', 4)};
 		for(Card card : cards) {
 			System.out.println(card);
 		}
@@ -12,10 +12,12 @@ public class Ex03_CardTestMain {
 		char[] kinds = {'♠', '◆', '♣', '♥'};
 		Card comCard = new Card(kinds[random.nextInt(4)], random.nextInt(13)+1);
 		System.out.println("컴퓨터가 랜덤하게 선택한 카드 : " +comCard);
-		if(cards[3].equals(comCard)) {
-			System.out.println("두 카드는 일치합니다");
-		}else {
-			System.out.println("두 카드는 불일치합니다");
+		for(int idx=0 ; idx<cards.length ; idx++) {
+			if(cards[idx].equals(comCard)) {
+				System.out.println(idx+"번째 카드와 일치 ★ ★ ★");
+			}else {
+				System.out.println(idx+"번째 카드와 불일치");
+			}
 		}
 	}
 }
