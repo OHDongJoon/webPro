3ac7b425e03a1001a6bdca993662d0981e23453d
unknown <spacenyi@naver.com>
2022-03-31 11:19:16 +0900

    03-31

--- source/01_java/ch14_api/src/com/lec/ex3_math/Ex04.java
+++ source/01_java/ch14_api/src/com/lec/ex3_math/Ex04.java
@@ -8,8 +8,17 @@ public class Ex04 {
 		for(int idx=0 ; idx<lotto.length ; idx++) {
 			lotto[idx] = random.nextInt(45)+1;
 		} // for - lotto번호 6개 추출
+		for(int i=0 ; i<lotto.length ; i++) {
+			for(int j=i+1 ; j<lotto.length ; j++) {
+				if(lotto[i] > lotto[j]) { // i번째 수가 j번째 수보다 크면 바꿈
+					int temp = lotto[i];
+					lotto[i] = lotto[j];
+					lotto[j] = temp;
+				}
+			}
+		} // 오름차순 정렬
 		for(int l : lotto) {
-			System.out.print(l + "\t");
+			System.out.print(l + " ");
 		}
 	}
 }
