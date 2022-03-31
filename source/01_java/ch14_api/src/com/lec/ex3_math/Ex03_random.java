6b6e14fc92efda68c5aa95c04b79eb06fa498f0d
unknown <spacenyi@naver.com>
2022-03-31 10:43:56 +0900

    03-31

---
+++ source/01_java/ch14_api/src/com/lec/ex3_math/Ex03_random.java
@@ -0,0 +1,10 @@
+package com.lec.ex3_math;
+public class Ex03_random {
+	public static void main(String[] args) {
+		System.out.println("0~ 45미만의 double 난수 : " + Math.random()*45);
+		System.out.println("0~ 45미만의 정수 난수 : " + (int)(Math.random()*45) );
+		System.out.println("로또 번호 뽑기 : " + (int)(Math.random()*45)+1);
+		/* 0 <= Math.random() < 1
+		 * 0 <= Math.random()*45 < 45 */
+	}
+}
