ad6ea482dd1235382f3bfbeefbb0b216a5ac21ec
unknown <spacenyi@naver.com>
2022-03-31 17:25:00 +0900

    03-31

--- source/01_java/ch14_api/src/com/lec/ex4_object/Card.java
+++ source/01_java/ch14_api/src/com/lec/ex4_object/Card.java
@@ -12,4 +12,23 @@ public class Card {
 		return "카드는 " + kind + ", " + num;
 	}
 	// c1.equals(c2) => c1의 kind와 c2의 kind, c1의 num와 c2의 num을 비교
+	@Override
+	public boolean equals(Object obj) {
+		// c1.equals(c2) => c1은 내객체, c2는 obj. 
+		if(obj!=null && obj instanceof Card) {//내객체의 kind와 obj.kind비교, 내객체의 num과 obj.num을 비교
+			boolean kindChk =  ( kind == ((Card)obj).kind );
+			boolean numChk  =  (num   == ((Card)obj).num);
+			return kindChk && numChk ;
+			//return ( kind == ((Card)obj).kind ) && (num   == ((Card)obj).num);
+		}
+		return false;
+	}
 }
+
+
+
+
+
+
+
+
--- source/01_java/ch14_api/src/com/lec/ex4_object/Ex03_CardTestMain.java
+++ source/01_java/ch14_api/src/com/lec/ex4_object/Ex03_CardTestMain.java
@@ -1,5 +1,6 @@
 package com.lec.ex4_object;
-
+import java.util.Random;
+// comCard는 랜덤카드 선택하여 배열방 0~끝방까지 일치하는지 불일치 하는지 구현해 봅니다.
 public class Ex03_CardTestMain {
 	public static void main(String[] args) {
 		Card[] cards = {new Card('♧', 1),  new Card('◇', 2), 
@@ -7,6 +8,14 @@ public class Ex03_CardTestMain {
 		for(Card card : cards) {
 			System.out.println(card);
 		}
-		Card comPu
+		Random random = new Random();
+		char[] kinds = {'♠', '◆', '♣', '♥'};
+		Card comCard = new Card(kinds[random.nextInt(4)], random.nextInt(13)+1);
+		System.out.println("컴퓨터가 랜덤하게 선택한 카드 : " +comCard);
+		if(cards[3].equals(comCard)) {
+			System.out.println("두 카드는 일치합니다");
+		}else {
+			System.out.println("두 카드는 불일치합니다");
+		}
 	}
 }
