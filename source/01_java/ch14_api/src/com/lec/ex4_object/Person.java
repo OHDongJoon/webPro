852aa0c89d7d0a450c7c7d7d2b213d87eb83e003
unknown <spacenyi@naver.com>
2022-03-31 16:48:01 +0900

    03-31

--- source/01_java/ch14_api/src/com/lec/ex4_object/Person.java
+++ source/01_java/ch14_api/src/com/lec/ex4_object/Person.java
@@ -8,4 +8,14 @@ public class Person {
 	public String toString() {
 		return "Person [juminNo=" + juminNo + "]";
 	}
+	@Override
+	public boolean equals(Object obj) { 
+		// p1.equals(p2)호출할 경우 p1대신 this, p2대신 obj => 내객체의 juminNo와 obj.juminNo가 같은지 여부를 return
+		// p1.equals(p3)호출할 경우 p3대신 obj => false를 return
+		if(obj !=null && obj instanceof Person) { // obj가 Person 타입의 객체
+			return juminNo == ((Person)obj).juminNo;
+		}else {
+			return false;
+		}// if
+	}// equals
 }
