23d6d6245a2cbe16d76cb5198eb1f87dc5b5e873
unknown <spacenyi@naver.com>
2022-03-31 11:47:29 +0900

    03-31

--- source/01_java/ch14_api/src/com/lec/ex3_math/Ex05_lotto.java
+++ source/01_java/ch14_api/src/com/lec/ex3_math/Ex05_lotto.java
@@ -6,7 +6,19 @@ public class Ex05_lotto {
 		int[] lotto = new int[6];
 		Random random = new Random();
 		for(int idx=0 ; idx<lotto.length ; idx++) {
-			lotto[idx] = random.nextInt(45)+1;
+			//lotto[idx] = random.nextInt(45)+1;
+			int temp = random.nextInt(45)+1; // 뽑은 난수를 임시 변수에 저장하고 
+			boolean ok = true; // 뽑은 난수가 중복이 아닐 경우 true/ 중복일 경우 false
+			for(int i=0 ; i<idx ; i++) {
+				if(temp == lotto[i]) { // 뽑은 난수가 중복이니 다시 뽑아
+					idx --; 
+					ok = false;
+					break;
+				}
+			} // for : 발생된 난수가 이전 앞방에 저장되었는지 확인(중복된 난수인지)
+			if(ok) {
+				lotto[idx] = temp;
+			}
 		} // for - lotto번호 6개 추출
 		for(int i=0 ; i<lotto.length ; i++) {
 			for(int j=i+1 ; j<lotto.length ; j++) {
