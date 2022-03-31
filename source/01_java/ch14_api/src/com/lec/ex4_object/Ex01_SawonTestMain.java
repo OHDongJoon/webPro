755d7e52180403081b0f31768d0ad409ba615c05
unknown <spacenyi@naver.com>
2022-03-31 13:59:47 +0900

    03-31

--- source/01_java/ch14_api/src/com/lec/ex4_object/Ex01_SawonTestMain.java
+++ source/01_java/ch14_api/src/com/lec/ex4_object/Ex01_SawonTestMain.java
@@ -3,7 +3,7 @@ public class Ex01_SawonTestMain {
 	public static void main(String[] args) {
 		Sawon s1 = new Sawon("a01", "홍길동", Dept.COMPUTER);
 		Sawon s2 = new Sawon("a02", "신길동", Dept.HUMANRESOURCES, 2022, 3, 24);
-		System.out.println(s1.toString()); // [사번]a01 [이름]홍길동 [부서]COMPUTER [입사일]2022년3월24일(목)
+		System.out.println(s1); // [사번]a01 [이름]홍길동 [부서]COMPUTER [입사일]2022년3월24일(목)
 		System.out.println(s2);
 	}
 }
