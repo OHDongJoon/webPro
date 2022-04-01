c136e4fd5cd7418af54e0d605ba3e9016d5341a2
unknown <spacenyi@naver.com>
2022-04-01 18:02:41 +0900

    04-01

--- source/01_java/ch15_exception/src/com/lec/ex4_newexception/Account.java
+++ source/01_java/ch15_exception/src/com/lec/ex4_newexception/Account.java
@@ -18,9 +18,14 @@ public class Account {
 		System.out.println("예금전 : " + this);
 		balance += amount; // balance = balance + amount; 예금
 		System.out.println(amount + "원 예금후 : " + this);
-	} // 출금 : 출금전 계좌정보 출력 -> 출금 -> 출금후 계좌정보 출력
-	public void withdraw(int amount) {
-		
+	} // 출금 : 잔액확인 -> 출금전 계좌정보 출력 -> 출금 -> 출금후 계좌정보 출력
+	public void withdraw(int amount) throws Exception {
+		if(balance<amount) { // 잔액부족으로 출금되지 않음 : 강제 예외발생
+			throw new Exception(amount + "원 출금하기에는 잔액("+balance+")이 부족합니다");
+		}
+		System.out.println("출금전 : " + this);
+		balance -= amount; // balance = balance - amount; 출금
+		System.out.println(amount + "원 출금후 : "+ this);
 	}
 	@Override
 	public String toString() {
