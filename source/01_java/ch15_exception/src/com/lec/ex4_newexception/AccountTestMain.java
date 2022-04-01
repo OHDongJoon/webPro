e776d33e63c4a0ad727432b7b1b86d38ae6b921b
unknown <spacenyi@naver.com>
2022-04-01 18:17:54 +0900

    04-01

---
+++ source/01_java/ch15_exception/src/com/lec/ex4_newexception/AccountTestMain.java
@@ -0,0 +1,19 @@
+package com.lec.ex4_newexception;
+public class AccountTestMain {
+	public static void main(String[] args) {
+		Account obj1 = new Account("111-111", "홍길동");
+		Account obj2 = new Account("222-222", "신길동", 2000);
+		obj1.deposit(1000);
+		obj2.deposit(1000);
+		try {
+			obj1.withdraw(500);
+		} catch (Exception e) {
+			System.out.println("예외메세지 : "+e.getMessage());
+		} // withdraw 메소드 선언시 throws가 있으면 호출시에도 try-catch
+		try {
+			obj2.withdraw(30000);
+		} catch (Exception e) {
+			System.out.println("예외메세지 : "+e.getMessage());
+		}
+	}
+}
