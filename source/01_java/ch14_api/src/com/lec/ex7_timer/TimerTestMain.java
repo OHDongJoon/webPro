6f5f945fcb90b97202b30067579b4b988dc2eb00
unknown <spacenyi@naver.com>
2022-04-01 13:54:28 +0900

    04-01

--- source/01_java/ch14_api/src/com/lec/ex7_timer/TimerTestMain.java
+++ source/01_java/ch14_api/src/com/lec/ex7_timer/TimerTestMain.java
@@ -7,9 +7,10 @@ public class TimerTestMain {
 		Timer timer = new Timer(true); // 프로그램이 끝나도 계속 timer상주
 		TimerTask task1 = new TimerTaskEx1();
 		TimerTask task2 = new TimerTaskEx2();
-		timer.schedule(task1, 2000); // 2초후에 task1수행
-		// 3초동안 잠깐 멈춤 
-		Thread.sleep(3000);
+		timer.schedule(task1, 2000); // 2초후에 task1.run() 수행
+		timer.schedule(task2, 1000, 500); // 1초후 0.5초마다 task2.run() 수행
+		// 5초동안 잠깐 멈춤 
+		Thread.sleep(5000);
 		System.out.println("끝");
 	}
 }
