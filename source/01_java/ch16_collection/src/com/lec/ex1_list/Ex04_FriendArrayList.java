c2465cd4a0c36b1efe4a809c21024cf65e02de21
Soyoung-Yi <spacenyi@naver.com>
2022-04-04 15:49:07 +0900

    04-04

--- source/01_java/ch16_collection/src/com/lec/ex1_list/Ex04_FriendArrayList.java
+++ source/01_java/ch16_collection/src/com/lec/ex1_list/Ex04_FriendArrayList.java
@@ -1,7 +1,36 @@
 package com.lec.ex1_list;
+import java.util.ArrayList;
+import java.util.Date;
 
+import com.lec.ex2_map.Friend;
 public class Ex04_FriendArrayList {
 	public static void main(String[] args) {
-		
+		Friend[] friends = new Friend[5];
+		friends[0]  = new Friend("È«", "010-9999-9999");
+		friends[2]  = new Friend("±è", "010-8888-8888");
+		for(int i=0 ; i<friends.length ; i++) {
+			System.out.println(friends[i]);
+		}
+		/** ArrayList : index°¡ ÀÖ°í, Áßº¹ Çã¿ë ***/
+		ArrayList<Friend> fs = new ArrayList<Friend>();
+		fs.add(new Friend("È«", "010-9999-9999")); // 0¹ø ÀÎµ¦½º
+		fs.add(new Friend("±è", "010-8888-8888")); // 1¹ø ÀÎµ¦½º
+		Friend temp = new Friend("ÀÌ", "010-7777-7777", new Date(95,11,12));
+		fs.add(temp); // 2¹ø ÀÎµ¦½º
+		for(int i=0 ; i<fs.size() ; i++) {
+			System.out.println(fs.get(i));
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
