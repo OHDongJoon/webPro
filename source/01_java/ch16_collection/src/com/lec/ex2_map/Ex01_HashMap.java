7b6962bce1b77f41d5ac93a8f9ac3321cb8cf1e9
Soyoung-Yi <spacenyi@naver.com>
2022-04-04 14:59:41 +0900

    04-04

--- source/01_java/ch16_collection/src/com/lec/ex2_map/Ex01_HashMap.java
+++ source/01_java/ch16_collection/src/com/lec/ex2_map/Ex01_HashMap.java
@@ -1,13 +1,48 @@
 package com.lec.ex2_map;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 public class Ex01_HashMap {
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("aaa"); 
 		System.out.println(list.get(0)); // list계열의 collection은 index로 get함
 		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
-		hashMap.put(11, "str11"); // 11key값에 데이터 "str11" 추가
+		hashMap.put(11, "str11"); // 11key값에 매핑되는 데이터 "str11" 추가
+		hashMap.put(20, "str20"); // 20 key에 매핑되는 데이터 "str20"추가
+		hashMap.put(33, "str33");
+		System.out.println(hashMap.get(20)); // key값으로 데이터 get함
+		System.out.println("remove 전 : " + hashMap);
+		hashMap.remove(20); // 20 key와 값이 remove
+		System.out.println("remove 후 : " + hashMap);
+		hashMap.clear();
+		System.out.println(hashMap.isEmpty()? "데이터 모두 삭제" : "데이터 있음");
+		hashMap.put(0, "Hong gildong");
+		hashMap.put(10, "Kim dongil");
+		hashMap.put(22, "Lee soonsin");
+		hashMap.put(40,  "Yu ain");
 		System.out.println(hashMap);
+		Iterator<Integer> iterator = hashMap.keySet().iterator();
+		while(iterator.hasNext()) {
+			Integer key = iterator.next();
+			System.out.println(key + "의 데이터는 "+ hashMap.get(key));
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
