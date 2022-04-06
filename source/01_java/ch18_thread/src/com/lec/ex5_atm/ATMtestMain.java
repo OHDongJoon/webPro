b9f08d14ac151bd2cb3c4662d27713185c6d8769
Soyoung-Yi <spacenyi@naver.com>
2022-04-06 14:24:32 +0900

    04-06

---
+++ source/01_java/ch18_thread/src/com/lec/ex5_atm/ATMtestMain.java
@@ -0,0 +1,11 @@
+package com.lec.ex5_atm;
+public class ATMtestMain {
+	public static void main(String[] args) {
+		ATM card = new ATM(20000);
+		Runnable target = new ATM_Target(card);
+		Thread th1 = new Thread(target, "MOM");
+		Thread th2 = new Thread(target, "DAD");
+		th1.start();
+		th2.start();
+	}
+}
