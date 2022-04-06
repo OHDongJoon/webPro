3cf8565894dbb287f4bce02ae03df5a4dfc6726d
Soyoung-Yi <spacenyi@naver.com>
2022-04-06 13:44:35 +0900

    04-06

--- source/01_java/ch18_thread/src/com/lec/ex3_threadNObject1/TargetEx.java
+++ source/01_java/ch18_thread/src/com/lec/ex3_threadNObject1/TargetEx.java
@@ -4,14 +4,17 @@ public class TargetEx implements Runnable {
 	@Override
 	public void run() {
 		for(int i=0 ; i<10 ; i++) {
-			if(Thread.currentThread().getName().equals("A")) {// "A"쓰레드일 경우
-				System.out.println("~ ~ ~ A 쓰레드 수행중 ~ ~ ~");
-				num++;
-			}
-			System.out.println(Thread.currentThread().getName() +"의 num = " + num);
+			out();
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) { }
 		}//for
 	}//run()
+	private void out() {
+		if(Thread.currentThread().getName().equals("A")) {// "A"쓰레드일 경우
+			System.out.println("~ ~ ~ A 쓰레드 수행중 ~ ~ ~");
+			num++;
+		}
+		System.out.println(Thread.currentThread().getName() +"의 num = " + num);
+	}
 }//class
