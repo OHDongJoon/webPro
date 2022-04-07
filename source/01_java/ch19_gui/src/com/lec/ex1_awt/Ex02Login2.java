540325c2ef9766d72fecef61713be567b312be70
Soyoung-Yi <spacenyi@naver.com>
2022-04-07 10:46:38 +0900

    04-07

--- source/01_java/ch19_gui/src/com/lec/ex1_awt/Ex02Login2.java
+++ source/01_java/ch19_gui/src/com/lec/ex1_awt/Ex02Login2.java
@@ -1,39 +1,48 @@
 package com.lec.ex1_awt;
+import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.Label;
+import java.awt.Panel;
 import java.awt.TextField;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-public class Ex02Login extends Frame{
+public class Ex02Login2 extends Frame{
+	private Panel     panel;
 	private Label     lbl1;
 	private TextField txtId;
 	private Label     lbl2;
 	private TextField txtPw;
 	private Button    btnLogin;
-	public Ex02Login(String title) { // Frame에 컴포넌트 추가하고 화면 보이게 -> 이벤트
+	public Ex02Login2(String title) { // Frame에 컴포넌트 추가하고 화면 보이게 -> 이벤트
 		super(title);
 		// Frame의 레이아웃 타입 : BorderLayout (동, 서, 남, 북, CENTER) 기본값
 		//                    FlowLayout (왼쪽부터 오른쪽으로, 위부터 아래로 차곡차곡 컴포넌트 add)
 		//                    GridLayout (몇행몇열)
-		setLayout(new FlowLayout()); // Frame의 레이아웃 셋팅
+		panel = new Panel(); // 패널의 기본 레이아웃 : FlowLayout
+		//setLayout(new GridLayout(2, 2));
+		
 		lbl1  = new Label("아 이 디");
 		txtId = new TextField("ID",20);
 		lbl2  = new Label("비밀번호");
 		txtPw = new TextField(20);
 		txtPw.setEchoChar('*');
 		btnLogin = new Button("LOGIN");
-		add(lbl1);
-		add(txtId);
-		add(lbl2);
-		add(txtPw);
-		add(btnLogin);
+		
+		panel.add(lbl1);
+		panel.add(txtId);
+		panel.add(lbl2);
+		panel.add(txtPw);
+		panel.setPreferredSize(new Dimension(280, 50));
+		
+		add(panel, BorderLayout.NORTH);
+		add(btnLogin, BorderLayout.SOUTH);
 		setSize(new Dimension(300, 150));
 		setLocation(200,100);
 		setVisible(true);
-		setResizable(false); // 사용자가 size 조정 불가
 		// x 클릭시 종료 
 		addWindowListener(new WindowAdapter() {
 			@Override
@@ -44,11 +53,11 @@ public class Ex02Login extends Frame{
 			}
 		});
 	}
-	public Ex02Login() {
+	public Ex02Login2() {
 		this("");
 	}
 	public static void main(String[] args) {
-		new Ex02Login("Login");
+		new Ex02Login2("Login");
 	}
 }
 
