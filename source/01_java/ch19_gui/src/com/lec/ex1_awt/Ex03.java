a3037e48e697bf716b94f9ccedf8811d7a7077d3
Soyoung-Yi <spacenyi@naver.com>
2022-04-07 11:28:44 +0900

    04-07

--- source/01_java/ch19_gui/src/com/lec/ex1_awt/Ex03.java
+++ source/01_java/ch19_gui/src/com/lec/ex1_awt/Ex03.java
@@ -1,8 +1,17 @@
 package com.lec.ex1_awt;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.List;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-public class Ex03 extends Frame {
+public class Ex03 extends Frame implements ActionListener{
 	private Panel     panel;
 	private TextField txtField;
 	private Button    btnOk, btnExit;
@@ -33,10 +42,29 @@ public class Ex03 extends Frame {
 				System.exit(0);    // 강제 종료
 			}
 		});
+		// 이벤트 추가 1. implements ActionListener -> override
+		//         2. 이벤트 리스너 추가 (V)
+		btnOk.addActionListener(this);
+		btnExit.addActionListener(this);
+		//         3. override된 메소드 안에 로직추가
 	}
 	public Ex03(String title) {
 		this();
 		setTitle(title);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnOk) { // OK버튼 클릭시 할 로직 : txtField의 텍스트를 list 추가
+			list.add(txtField.getText().trim().toUpperCase());
+			txtField.setText("");
+		}else if(e.getSource() == btnExit) { // 종료 로직
+			setVisible(false); // 안보이게
+			dispose();         // 자원 해제
+			System.exit(0);    // 강제 종료
+		}
+	}
+	public static void main(String[] args) {
+		new Ex03();
 	}
 }
 
