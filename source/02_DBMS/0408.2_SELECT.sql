c67dba0f5b0346658c857e0035fb91e9257b9acc
Soyoung-Yi <spacenyi@naver.com>
2022-04-08 16:35:30 +0900

    04-07

--- source/02_DBMS/0408.2_SELECT.sql
+++ source/02_DBMS/0408.2_SELECT.sql
@@ -1,2 +1,6 @@
--- [II] SELECT 한글이 
-SELECT
+-- [II] SELECT 문 - 가장 많이 쓰이는 DML(Data Manipulation Language) ; 검색 기능
+-- 1. SQL SELECT문장 작성법
+-- 현재 계정 (실행 : ctrl + Enter)
+SHOW USER; 
+SELECT * FROM TAB; -- 현 계정(scott)이 가지고 있는 테이블
+SELECT * FROM EMP; -- EMP 테이블의 모든 열(필드), 모든 행
