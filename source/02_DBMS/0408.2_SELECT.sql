622daaa9602876e49306447c381b8b1cc6b08a77
Soyoung-Yi <spacenyi@naver.com>
2022-04-08 17:31:46 +0900

    04-07

--- source/02_DBMS/0408.2_SELECT.sql
+++ source/02_DBMS/0408.2_SELECT.sql
@@ -17,3 +17,35 @@ SELECT EMPNO 사번, ENAME 이름, SAL 급여, JOB FROM EMP;
 SELECT EMPNO "사 번", ENAME "이 름", SAL "급 여", JOB FROM EMP;
 SELECT EMPNO NO, ENAME NAME, SAL SALARY FROM EMP; -- TITLE : NO, NAME, SALARY
 
+-- 3. 특정 행 출력 : WHERE 절(조건)-비교연산자 ; 같다(=), 크거나같다(>=).. 다르다(!=, ^=, <>)
+SELECT EMPNO "사번", ENAME "이름", SAL "급여" FROM EMP WHERE SAL = 3000; -- 같다
+SELECT EMPNO "사번", ENAME "이름", SAL "급여" FROM EMP WHERE SAL != 3000; -- 다르다
+SELECT EMPNO "사번", ENAME "이름", SAL "급여" FROM EMP WHERE SAL ^= 3000; -- 다르다
+SELECT EMPNO "사번", ENAME "이름", SAL "급여" FROM EMP WHERE SAL <> 3000; -- 다르다
+    -- 비교연산자는 숫자, 문자, 날짜형 모두 가능
+    -- ex. 사원이름(ENAME)이 'A','B','C'로 시작하는 사원의 모든 필드
+SELECT * FROM EMP WHERE ENAME < 'D';
+    -- ex. 81년도 이전에 입사한 사원의 모든 필드
+SELECT * FROM EMP WHERE HIREDATE < '81/01/01';
+    -- ex. 10번 부서번호(deptno)인 사원의 모든 필드를 출력
+SELECT * FROM EMP WHERE DEPTNO=10;
+    -- ex. 이름(ENAME)이 FORD인 직원의 EMPNO, ENAME, MGR(상사의 사번)을 출력
+SELECT EMPNO, ENAME, MGR FROM EMP WHERE ENAME = 'FORD';
+select empno, ename, mgr from emp where ename = 'FORD'; -- 데이터는 대소문자 구분
+
+-- 4. 논리연산자 : AND, OR
+    -- ex. 급여(SAL)가 2000이상 3000이하인 직원의 모든 필드
+SELECT * FROM EMP WHERE SAL>=2000 AND SAL<=3000;
+    -- ex. 82년도에 입사한 사원의 모든 필드
+SELECT * FROM EMP WHERE HIREDATE >= '82/01/01' AND HIREDATE<= '82/12/31';
+-- 날짜 표기법 셋팅 (현재 : YY/MM/DD OR RR/MM/DD)
+ALTER SESSION SET NLS_DATE_FORMAT = 'MM-DD-RR';
+ALTER SESSION SET NLS_DATE_FORMAT = 'RR/MM/DD'; -- 한글윈도우즈 위에 INSTALL
+SELECT ENAME, HIREDATE FROM EMP;
+    -- 연봉이 2400 이상인 직원의 ENAME, SAL, 연봉 출력 (연봉 = SAL*12)
+SELECT ENAME, SAL, SAL*12 "연봉" FROM EMP WHERE SAL*12 > 2400;
+SELECT ENAME, SAL, SAL*12 "연봉" FROM EMP WHERE 연봉>2400; -- WHERE절에는 별칭을 쓸 수 없다
+
+
+
+
